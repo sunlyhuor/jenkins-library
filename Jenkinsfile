71a984ddf84f6b7dd863710701811c4f62b184ce
sunlyhuor <jkhmer1111@gmail.com>
2024-06-11 10:22:49 +0700

    init

---
+++ Jenkinsfile
@@ -0,0 +1,9 @@
+@Library('buildAndDeploy') _
+
+buildAndDeploy(
+    image: 'react-docker',
+    registry: 'leehuor',
+    tag: '1',
+    containerPort: '3001',
+    hostPort: '3001'
+)
---
+++ vars/buildAndDeploy.groovy
@@ -0,0 +1,45 @@
+def call(Map config = [:]) {
+    // Default values
+    def image = config.get('image', 'my-default-image')
+    def registry = config.get('registry', 'my-default-registry')
+    def tag = config.get('tag', 'latest')
+    def containerPort = config.get('containerPort', '8080')
+    def hostPort = config.get('hostPort', '8080')
+
+    pipeline {
+        agent any
+
+        stages {
+            stage('Build Docker Image') {
+                steps {
+                    script {
+                        echo "Building Docker image: ${registry}/${image}:${tag}"
+                        sh """
+                            docker build -t ${registry}/${image}:${tag} .
+                            docker push ${registry}/${image}:${tag}
+                        """
+                    }
+                }
+            }
+
+            stage('delete container'){
+                steps{
+                    sh'''
+                    docker rm -f test
+                    '''
+                }
+            }
+
+            stage('Deploy Docker Container') {
+                steps {
+                    script {
+                        echo "Deploying Docker container: ${registry}/${image}:${tag}"
+                        sh """
+                            docker run -d --name test --restart=always -p ${hostPort}:${containerPort} ${registry}/${image}:${tag}
+                        """
+                    }
+                }
+            }
+        }
+    }
+}
